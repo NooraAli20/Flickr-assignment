7519fc38d43a61fc932381725997858528721803
NooraAli20 <noora_ali89@hotmail.com>
2020-12-08 20:22:49 +0100

    different commits

---
+++ index.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+const searchQuery = document.querySelector('#query');
+const imgList = document.querySelector('#images');
+const baseUrl = 'https://api.flickr.com/services/rest?method=flickr.photos.search';
+const APIkey = 'a498e6e237c5d3a3c29ab672a224df95';
+console.log('images')
+
+
+
+// https://api.flickr.com/services/rest?method=flickr.photos.search//
